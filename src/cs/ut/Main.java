5eb220758f2d86be9badeaaf10d7736e5e166a2c
Markus Leemet <markusleemet@gmail.com>
2020-08-12 16:35:25 +0300

    Create mothod to read user input

--- src/cs/ut/Main.java
+++ src/cs/ut/Main.java
@@ -1,8 +1,25 @@
 package cs.ut;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /**
+     * Method asks for user input and returns it as a String
+     * @return user input as a string
+     * @throws IOException
+     */
+    static String readUserInput() throws IOException {
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        return bufferedReader.readLine();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String userInput = readUserInput();
+        System.out.println(userInput);
     }
 }
